93576f7f9f16284528901555f464a7e90ee3be90
Pete Richardson <pete@peterichardson.com>
2024-11-27 10:39:57 -0800

    Add a colored circle behind the details

--- enviro1/ContentView.swift
+++ enviro1/ContentView.swift
@@ -24,23 +24,29 @@ struct ContentView: View {
             } detail: {
                 if  let person = viewModel.selectedPerson {
                     VStack {
-                        Image(systemName: viewModel.imageForGender(person.gender))
-                                .resizable()
+                        ZStack {
+                            Circle()
                                 .foregroundColor(viewModel.colorForGender(person.gender))
-                                .scaledToFit()
-                                .frame(width:75, height: 75)
-                            Text("\(person.name)").font(.largeTitle)
-                            Text("\(person.age) \(person.gender)")
-                    }
-                    .padding()
-                    HStack {
-                        Button("Transition") {
-                            person.transition(to: nil)
+                                .padding()
+                            VStack {
+                                Image(systemName: viewModel.imageForGender(person.gender))
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width:75, height: 75)
+                                Text("\(person.name)").font(.largeTitle)
+                                Text("\(person.age) \(person.gender)")
+                            }
+                            .padding()
                         }
-                        Button("Birthday") {
-                            person.birthday()
+                        HStack {
+                            Button("Transition") {
+                                person.transition(to: nil)
+                            }
+                            Button("Birthday") {
+                                person.birthday()
+                            }
                         }
-                    }
+                    }.padding()
                 }
                 else {
                     Text("Select a person")
